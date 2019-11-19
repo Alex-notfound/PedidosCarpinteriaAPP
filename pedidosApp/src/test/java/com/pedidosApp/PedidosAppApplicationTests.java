2710b2ee964cecc03627c000f3090b8fff1cbbe5
Alexandre Currás Rodríguez <alexcurrasr@gmail.com>
2019-11-20 00:47:26 +0100

    Creacion proyecto en Eclipse

---
+++ pedidosApp/src/test/java/com/pedidosApp/PedidosAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pedidosApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PedidosAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
