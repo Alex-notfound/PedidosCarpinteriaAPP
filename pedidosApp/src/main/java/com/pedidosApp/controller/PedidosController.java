a5a393dfc2e797699e0df33622709788d7038f08
Alexandre Currás Rodríguez <alexcurrasr@gmail.com>
2020-02-07 19:25:32 +0100

    Updated view of PedidosList and implemented Delete with modal

--- pedidosApp/src/main/java/com/pedidosApp/controller/PedidosController.java
+++ pedidosApp/src/main/java/com/pedidosApp/controller/PedidosController.java
@@ -3,19 +3,40 @@ package com.pedidosApp.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.pedidosApp.service.PedidoService;
 
 @Controller
+@RequestMapping("/pedidos")
 public class PedidosController {
 
 	@Autowired
-	private PedidoService perdidoService;
+	private PedidoService pedidoService;
 
 	@RequestMapping(value = { "", "/" })
-	public String index(Model model) {
-		model.addAttribute("list", perdidoService.getAll());
+	public String listAll(Model model) {
+		model.addAttribute("list", pedidoService.getAll());
 		return "PedidosList";
+	}
+
+	@GetMapping("/detail/{id}")
+	public String detail(Model model) {
+		// TODO: detail
+		return "PedidoDetail";
+	}
+
+	@GetMapping("/edit/")
+	public String edit(Model model) {
+		// TODO: edit
+		return "PedidoForm";
+	}
+
+	@GetMapping("/delete/{id}")
+	public String delete(@PathVariable("id") Long id, Model model) {
+		pedidoService.delete(id);
+		return listAll(model);
 	}
 }
