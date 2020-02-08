570fdcf926d655223f598d4c915493a110264f44
Alexandre Currás Rodríguez <alexcurrasr@gmail.com>
2020-02-08 22:27:14 +0100

    Create MuebleRepository.java

---
+++ pedidosApp/src/main/java/com/pedidosApp/repository/MuebleRepository.java
@@ -0,0 +1,9 @@
+package com.pedidosApp.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.pedidosApp.model.Mueble;
+
+public interface MuebleRepository extends JpaRepository<Mueble, Long> {
+
+}
